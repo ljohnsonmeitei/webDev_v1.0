c6f844efcac530af9fb5ff2ee364e98a1036cd0d
Johnson <49550500+ljohnsonmeitei@users.noreply.github.com>
2024-01-15 06:16:20 +0530

    Create Async.js

---
+++ week_02/2.1/Async.js
@@ -0,0 +1,12 @@
+//readfile function
+//setTimeout
+function onDone(){
+  console.log("Hi there");
+}
+
+setTimeout(onDone,1000);
+
+console.log("after settimeout")
+
+for(let i=0;i<1000000000;i++){
+}
